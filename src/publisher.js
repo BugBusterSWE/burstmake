7ec2467d4929c571d6cef8e3484ce5313b6dd762
korut94 <andreamnt94@gmail.com>
2016-04-05 13:21:58 +0200

    Add section to manage tsconfig object and pusblish it

--- src/publisher.js
+++ src/publisher.js
@@ -1,10 +1,12 @@
+const readDir = require( "recursive-readdir-synchronous" );
+const fs = require( "fs" );
+const Leveler = require( "./leveler.js" );
+
 /**
 Create the hotpoint project make a tsconfig.json file with the option declared
 in the its topic. 
 */
 function Publisher( makeopt ) {
-    // Configuration for all hotpoint
-    this.makeopt = makeopt
 }
 
 /**
@@ -14,7 +16,39 @@ Object to get all information needed creating a tsconfig.json file
 @param hotpoint {string} Path where will creating the tsconfig.json file
 */
 Publisher.prototype.publish = function ( tsconfig, hotpoint )  {
-    console.log( tsconfig + " " + hotpoint );
+    var files = [];
+    var leveler = new Leveler();
+
+    try {
+	// Read all file inside directory specified in the hotpoint
+	files = readDir( hotpoint );
+    } catch ( e ) {
+	throw "Impossible read directory " + hotpoint + " specified";
+    }
+    
+    // Concat result with the files included in the attribute 'include'
+    // and with their path started at the hotpoint
+    tsconfig.include = leveler.leveling(
+	tsconfig.include.concat( files ),
+	hotpoint
+    );
+
+    // Rename 'include' in 'files'
+    tsconfig.files = tsconfig.include;
+    delete tsconfig.include;
+
+    try {
+	// Publish the tsconfig file!"
+	fs.writeFileSync(
+	    hotpoint + "tsconfig.json",
+	    JSON.stringify( tsconfig ),
+	    "utf-8"
+	);
+    } catch ( e ) {
+	throw "Impossible create tsconfig.json file in " +
+	    "the hotpoint "
+	    + hotpoint;
+    }
 };
 
 module.exports = Publisher;
