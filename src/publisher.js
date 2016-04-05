b92608debcf16a11a82269b966e13ab6d0e66b7e
korut94 <andreamnt94@gmail.com>
2016-04-05 21:24:00 +0200

    Change contract. Now it only publish the tsconfig

--- src/publisher.js
+++ src/publisher.js
@@ -1,12 +1,10 @@
-const readDir = require( "recursive-readdir-synchronous" );
 const fs = require( "fs" );
-const Leveler = require( "./leveler.js" );
 
 /**
 Create the hotpoint project make a tsconfig.json file with the option declared
 in the its topic. 
 */
-function Publisher( makeopt ) {
+function Publisher() {
 }
 
 /**
@@ -16,27 +14,6 @@ Object to get all information needed creating a tsconfig.json file
 @param hotpoint {string} Path where will creating the tsconfig.json file
 */
 Publisher.prototype.publish = function ( tsconfig, hotpoint )  {
-    var files = [];
-    var leveler = new Leveler();
-
-    try {
-	// Read all file inside directory specified in the hotpoint
-	files = readDir( hotpoint );
-    } catch ( e ) {
-	throw "Impossible read directory " + hotpoint + " specified";
-    }
-    
-    // Concat result with the files included in the attribute 'include'
-    // and with their path started at the hotpoint
-    tsconfig.include = leveler.leveling(
-	tsconfig.include.concat( files ),
-	hotpoint
-    );
-
-    // Rename 'include' in 'files'
-    tsconfig.files = tsconfig.include;
-    delete tsconfig.include;
-
     try {
 	// Publish the tsconfig file!"
 	fs.writeFileSync(
